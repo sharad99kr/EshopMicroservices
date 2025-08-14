84b2dbeb5f1978ba3835b2de992e54275db8b816
sharad <sharad99kr@gmail.com>
2025-08-14 16:15:11 -0400

    Added Product endpoint records, updated create product handler command with Product entity using cammand object

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndpoint.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndpoint.cs
@@ -1,6 +1,10 @@
-﻿namespace Catalog.API.Products.CreateProduct
+﻿using BuildingBlocks.CQRS;
+
+namespace Catalog.API.Products.CreateProduct
 {
     //API related operations
+    public record CreateProductRequest(string Name, List<string> Category, string Description, string ImageFile, decimal Price);
+    public record CreateProductResponse(Guid Id);
     public class CreateProductEndpoint
     {
     }
--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -1,17 +1,32 @@
-﻿using MediatR;
+﻿using BuildingBlocks.CQRS;
+using Catalog.API.Models;
+using System.Xml.Linq;
 
 namespace Catalog.API.Products.CreateProduct
 {
     public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price)
-            : IRequest<CreateProductResult>;
+            : ICommand<CreateProductResult>;
     public record CreateProductResult(Guid Id);
     //Business Logic operations
-    internal class CreateProductCommandHandler : IRequestHandler<CreateProductCommand, CreateProductResult>
+    internal class CreateProductCommandHandler 
+        : ICommandHandler<CreateProductCommand, CreateProductResult>
     {
-        public Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken) {
-            
+        public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken) {
+
             //Business logic to create a product
-            throw new NotImplementedException();
+
+            //create Product entity from command object
+            var product = new Product {
+                Name = command.Name,
+                Category = command.Category,
+                Description = command.Description,
+                ImageFile = command.ImageFile,
+                Price = command.Price,
+            };
+            //save to database
+
+            //return CreateProductResult result
+            return new CreateProductResult(Guid.NewGuid());
         }
     }
 }
