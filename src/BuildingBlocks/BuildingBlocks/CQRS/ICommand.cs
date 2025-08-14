656f9f4a6a636f66a59fc6e58c6fe19260f721fd
sharad <sharad99kr@gmail.com>
2025-08-14 01:12:56 -0400

    Added IQuery and ICommand interface aligning with CQRS architecture

---
+++ src/BuildingBlocks/BuildingBlocks/CQRS/ICommand.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+
+namespace BuildingBlocks.CQRS
+{
+    //this interface doesn't return a response
+    public interface ICommand: ICommand<Unit>
+    {
+    }
+
+    //this interface returns a response
+    public interface ICommand<out TResponse>: IRequest<TResponse>
+    {
+    }
+}
