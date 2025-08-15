9a9b783fb22ae919f17fe6f3837509856ca1e9b2
sharad <sharad99kr@gmail.com>
2025-08-14 23:57:52 -0400

    Added marten to the project and setup for postgres sql

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -1,6 +1,4 @@
-﻿using BuildingBlocks.CQRS;
-using Catalog.API.Models;
-using System.Xml.Linq;
+﻿
 
 namespace Catalog.API.Products.CreateProduct
 {
@@ -8,7 +6,7 @@ namespace Catalog.API.Products.CreateProduct
             : ICommand<CreateProductResult>;
     public record CreateProductResult(Guid Id);
     //Business Logic operations
-    internal class CreateProductCommandHandler 
+    internal class CreateProductCommandHandler(IDocumentSession session) 
         : ICommandHandler<CreateProductCommand, CreateProductResult>
     {
         public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken) {
@@ -24,9 +22,10 @@ namespace Catalog.API.Products.CreateProduct
                 Price = command.Price,
             };
             //save to database
-
+            session.Store(product);
+            await session.SaveChangesAsync(cancellationToken);
             //return CreateProductResult result
-            return new CreateProductResult(Guid.NewGuid());
+            return new CreateProductResult(product.Id);
         }
     }
 }
--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,10 +1,18 @@
 var builder = WebApplication.CreateBuilder(args);
 
 //add services to the container
+
+//carter
 builder.Services.AddCarter();
+//mediatR
 builder.Services.AddMediatR(config => {
     config.RegisterServicesFromAssembly(typeof(Program).Assembly);
 });
+//marten
+builder.Services.AddMarten(opts => {
+    opts.Connection(builder.Configuration.GetConnectionString("Database")!);
+}).UseLightweightSessions();
+
 var app = builder.Build();
 //configgure the HTTP request pipeline
 app.MapCarter();
