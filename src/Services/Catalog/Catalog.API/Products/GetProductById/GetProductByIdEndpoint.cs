1f37a2acdb4f43709277eaf794e9a432f0d49cd0
sharad <sharad99kr@gmail.com>
2025-08-17 01:10:14 -0400

    Added medthod definition of AddRoutes under GetProductByIdEndpoint class

--- src/Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdEndpoint.cs
+++ src/Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdEndpoint.cs
@@ -1,6 +1,23 @@
-﻿namespace Catalog.API.Products.GetProductById
+﻿using Catalog.API.Products.GetProducts;
+
+namespace Catalog.API.Products.GetProductById
 {
-    public class GetProductByIdEndpoint
+    //public record GetProductByIdRequest();
+    public record GetProductByIdResponse(Product Product); //This parameter should be same as in GetProductByIdResult inside GetProductByIdHandler
+
+    public class GetProductByIdEndpoint : ICarterModule
     {
+        public void AddRoutes(IEndpointRouteBuilder app) {
+            app.MapGet("/products/{id}", async (Guid id, ISender sender) => {
+                var result = await sender.Send(new GetProductByIdQuery(id));
+                var response = result.Adapt<GetProductByIdResponse>();
+                return Results.Ok(response);
+            })
+            .WithName("GetProductById")
+            .Produces<GetProductsResponse>(StatusCodes.Status200OK)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .WithSummary("get product By Id")
+            .WithDescription("Get Product By Id");
+        }
     }
 }
