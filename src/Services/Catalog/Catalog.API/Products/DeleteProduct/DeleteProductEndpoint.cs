0cbcc5d3bd49685bfc6f4291264a67173caeb6e1
sharad <sharad99kr@gmail.com>
2025-08-20 22:36:55 -0400

    added method calls for delete product

---
+++ src/Services/Catalog/Catalog.API/Products/DeleteProduct/DeleteProductEndpoint.cs
@@ -0,0 +1,25 @@
+﻿
+using Catalog.API.Products.GetProducts;
+using MediatR;
+
+namespace Catalog.API.Products.DeleteProduct
+{
+    //public record DeleteProductsRequest(Guid Id);
+    public record DeleteProductResponse(bool IsSuccess);
+    public class DeleteProductEndpoint : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app) {
+            app.MapDelete("/products/{id}", async (Guid id, ISender sender) => {
+                var result = await sender.Send(new DeleteProductCommand(id));
+                var response = result.Adapt<DeleteProductResponse>();
+                return Results.Ok(response);
+            })
+            .WithName("DeleteProducts")
+            .Produces<DeleteProductResponse>(StatusCodes.Status200OK)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .ProducesProblem(StatusCodes.Status404NotFound)
+            .WithSummary("Delete products")
+            .WithDescription("Delete Products");
+        }
+    }
+}
---
+++ src/Services/Catalog/Catalog.API/Products/DeleteProduct/DeleteProductHandler.cs
@@ -0,0 +1,20 @@
+﻿using Catalog.API.Products.CreateProduct;
+using Catalog.API.Products.GetProductById;
+
+namespace Catalog.API.Products.DeleteProduct
+{
+    public record DeleteProductCommand(Guid Id)
+            : ICommand<DeleteProductResult>;
+    public record DeleteProductResult(bool IsSuccess);
+    internal class DeleteProductCommandHandler
+        (IDocumentSession session, ILogger<GetProductByIdQueryHandler> logger)
+        : ICommandHandler<DeleteProductCommand, DeleteProductResult>
+    {
+        public async Task<DeleteProductResult> Handle(DeleteProductCommand command, CancellationToken cancellationToken) {
+            logger.LogInformation("DeleteProductCommandHandler.Handle called with {@Query}", command);
+            session.Delete<Product>(command.Id);
+            await session.SaveChangesAsync(cancellationToken);
+            return new DeleteProductResult(true);
+        }
+    }
+}
