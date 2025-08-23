f334be7356f60c4c19e58c796563028968abe459
sharad <sharad99kr@gmail.com>
2025-08-23 01:28:03 -0400

    Removed inline exception and added custom exception handler

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,6 +1,5 @@
-using BuildingBlocks.Behaviors;
-using Microsoft.AspNetCore.Diagnostics;
-using Microsoft.AspNetCore.Mvc;
+
+using BuildingBlocks.Exceptions.Handler;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -26,29 +25,12 @@ builder.Services.AddMarten(opts => {
     opts.Connection(builder.Configuration.GetConnectionString("Database")!);
 }).UseLightweightSessions();
 
+builder.Services.AddExceptionHandler<CustomExceptionHandler>();
+
 var app = builder.Build();
 //configgure the HTTP request pipeline
 app.MapCarter();
 
-app.UseExceptionHandler(exceptionHandlerApp => {
-    exceptionHandlerApp.Run(async context => {
-        var exception = context.Features.Get<IExceptionHandlerFeature>()?.Error;
-        if(exception == null) { 
-            return;
-        }
+app.UseExceptionHandler(options => { });
 
-        var problemDetails = new ProblemDetails { 
-            Title=exception.Message,
-            Status=StatusCodes.Status500InternalServerError,
-            Detail=exception.StackTrace
-        };
-
-        var logger=context.RequestServices.GetRequiredService<ILogger<Program>>();
-        logger.LogError(exception, exception.Message);
-
-        context.Response.StatusCode=StatusCodes.Status500InternalServerError;
-        context.Response.ContentType="application/problem+json";
-        await context.Response.WriteAsJsonAsync(problemDetails);
-    });
-});
 app.Run();
