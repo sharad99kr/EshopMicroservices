4556db06ab0ce9a50157ceeddc9d143860e59dad
sharad <sharad99kr@gmail.com>
2025-09-15 16:10:38 -0400

    ported discount api and basket api to communicate on docker

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -30,6 +30,13 @@ builder.Services.AddStackExchangeRedisCache(options => {
 //Grpc services
 builder.Services.AddGrpcClient<DiscountProtoService.DiscountProtoServiceClient>(options => {
     options.Address = new Uri(builder.Configuration["GrpcSettings:DiscountUrl"]!);
+})
+.ConfigurePrimaryHttpMessageHandler(() => {
+    var handler = new HttpClientHandler {
+        ServerCertificateCustomValidationCallback=
+        HttpClientHandler.DangerousAcceptAnyServerCertificateValidator
+    };
+    return handler;
 });
 
 //cross-cutting services
