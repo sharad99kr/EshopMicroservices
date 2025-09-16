f23c44b0b2b33a88a24d331f6650ec94c3e1ca30
sharad <sharad99kr@gmail.com>
2025-09-16 01:43:03 -0400

    Ordering.Domain: Added rich domain model by adding create methods in the models

--- src/Services/Ordering/Ordering.Domain/Ordering.Domain/Models/Customer.cs
+++ src/Services/Ordering/Ordering.Domain/Ordering.Domain/Models/Customer.cs
@@ -7,5 +7,15 @@ namespace Ordering.Domain.Models
         public string Name { get; private set; } = default!;
         public string Email { get; private set; } = default!;
 
+        public static Customer Create(CustomerId id, string name, string email) {
+            ArgumentException.ThrowIfNullOrWhiteSpace(name);
+            ArgumentException.ThrowIfNullOrWhiteSpace(email);
+            var customer= new Customer();
+            customer.Name = name;
+            customer.Email = email;
+            customer.Id = id;
+            return customer;
+        }
+
     }
 }
