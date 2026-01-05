013079654a3ee7ed7ef88f397353015f8d5112e2
sharad <sharad99kr@gmail.com>
2026-01-05 01:39:56 -0500

    Added seed data for Products and Orders in SQL Server OrderDb using EFCore with app startup

--- src/Services/Ordering/Ordering.Infrastructure/Data/Extensions/DatabaseExtensions.cs
+++ src/Services/Ordering/Ordering.Infrastructure/Data/Extensions/DatabaseExtensions.cs
@@ -17,12 +17,28 @@ namespace Ordering.Infrastructure.Data.Extensions
 
         public static async Task SeedAsync(ApplicationDbContext context) { 
             await SeedCustomerAsync(context);
+            await SeedProductAsync(context);
+            await SeedOrderandItemsAsync(context);
         }
         public static async Task SeedCustomerAsync(ApplicationDbContext context) {
             if(!await context.Customers.AnyAsync()) {
                 await context.Customers.AddRangeAsync(InitialData.Customers);
                 await context.SaveChangesAsync();
             }
-        }     
+        }
+
+        public static async Task SeedProductAsync(ApplicationDbContext context) {
+            if(!await context.Products.AnyAsync()) {
+                await context.Products.AddRangeAsync(InitialData.Products);
+                await context.SaveChangesAsync();
+            }
+        }
+
+        public static async Task SeedOrderandItemsAsync(ApplicationDbContext context) {
+            if(!await context.Orders.AnyAsync()) {
+                await context.Orders.AddRangeAsync(InitialData.OrdersWithItems);
+                await context.SaveChangesAsync();
+            }
+        }
     }
 }
