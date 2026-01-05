e6e33b920a0a8ab02043bfeaac5025de655f8673
sharad <sharad99kr@gmail.com>
2026-01-05 00:28:59 -0500

    setup SQL server DB using Docker-compose file for multi container docker env. applied EF core migrations to SQL Server Db with Update-Database Command

---
+++ src/Services/Ordering/Ordering.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,18 @@
+﻿
+
+namespace Ordering.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructureServices
+            (this IServiceCollection services, IConfiguration configuration) {
+            var connectionString = configuration.GetConnectionString("Database");
+
+            //Add services to the container
+            services.AddDbContext<ApplicationDbContext>(options => {
+                options.UseSqlServer(connectionString);
+            });
+            return services;
+        }
+    }
+}
